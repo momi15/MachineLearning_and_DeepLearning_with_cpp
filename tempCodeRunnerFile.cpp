3ac2af87cd0dbb620a3eea92cb01105faed2e59b
momi15 <hanitarek247@gmail.com>
2024-10-07 18:31:22 +0200

    Aggiornamenti automatici

---
+++ tempCodeRunnerFile.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+    bool prova(int x) {
+        if(x<0)return false;
+        std::string a=std::to_string(x),b;
+        for(int i=a.size();i>=0;--i){
+            b.push_back(a[i]);
+        }
+        cout<<b;
+        return a==b;
+    }
+
+int main(){
+    string a="ciao";
+    cout<<prova(222);
+    return 0;
+}
