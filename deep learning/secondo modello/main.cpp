a05d12faa37923e2e47d62dbd48047f807bb58dc
momi15 <hanitarek247@gmail.com>
2024-11-23 12:13:27 +0100

    Aggiornamenti automatici

--- deep learning/secondo modello/main.cpp
+++ deep learning/secondo modello/main.cpp
@@ -2,8 +2,8 @@
 int main(){
     srand(0);
     size_t arch[]={2,2,1};
-    NN derivataCost=nn_alloc(arch,ARRAY_LEN(arch));
     NN nn=nn_alloc(arch,ARRAY_LEN(arch));
+    NN derivataCost=nn_alloc(arch,ARRAY_LEN(arch));
 
     nn_fill(nn,-1,1);
     std::vector<double> td_xor{
