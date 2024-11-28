978e5dc6f18908bd31112d02fef76b5da1b26220
momi15 <hanitarek247@gmail.com>
2024-11-28 19:56:26 +0100

    Aggiornamenti automatici

--- deep learning/secondo modello/main.cpp
+++ deep learning/secondo modello/main.cpp
@@ -38,7 +38,7 @@ int main(){
     };
     mat_copy(NN_INPUT(nn),mat_row(ti,1));
     NN_forward(nn);
-    double eps=1e-2,rate=1e-6;
+    double eps=1e-2,rate=1e-9;
     std::cout<<std::endl<<nn_cost(nn,ti,to)<<std::endl;
     for(size_t i=0;i<20;++i){
         double c=nn_cost(nn,ti,to);
