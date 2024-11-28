8caf34f59f49e7979ddcbd1ae57ac274cabcdf89
momi15 <hanitarek247@gmail.com>
2024-11-28 20:01:40 +0100

    Aggiornamenti automatici

--- deep learning/secondo modello/main.cpp
+++ deep learning/secondo modello/main.cpp
@@ -40,7 +40,7 @@ int main(){
     NN_forward(nn);
     double eps=1e-2,rate=1e-3;
     std::cout<<std::endl<<nn_cost(nn,ti,to)<<std::endl;
-    for(size_t i=0;i<10000;++i){
+    for(size_t i=0;i<100;++i){
         double c=nn_cost(nn,ti,to);
 //        nn_finite_diff(nn,b,eps,ti,to);
         NN_Dcost(derivataCost,ti,to);
