69cd17004fd84782119c08a3acc3a71d3d231e3c
momi15 <hanitarek247@gmail.com>
2024-12-02 13:36:28 +0100

    Aggiornamenti automatici

--- ml/and or model/main.cpp
+++ ml/and or model/main.cpp
@@ -74,12 +74,14 @@ int main() {
     float rate = 1e-3;
     float c = cost(w1, w2,b);
     cout<<"C="<<c<<endl;
-    for(size_t i=0;i<100;++i){
+    for(size_t i=0;i<1000;++i){
         gcost(w1,w2,b,dw1,dw2,db);
         w1-=rate*dw1;
         w2-=rate*dw2;
         b-=rate*db;
     }
+    c = cost(w1, w2,b);
+    cout<<"C="<<c<<endl;
     for(int i=0;i<2;++i){
         for(int j=0;j<2;++j){
             cout<<i<<"|"<<j<<"="<<sigmoid(i*w1+j*w2+b)<<endl;
