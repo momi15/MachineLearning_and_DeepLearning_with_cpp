a3f6b8ae87acec4f8e9350bf725a3ae64cfe1cf9
momi15 <hanitarek247@gmail.com>
2024-12-02 13:53:06 +0100

    Aggiornamenti automatici

--- ml/and or model/main.cpp
+++ ml/and or model/main.cpp
@@ -66,7 +66,7 @@ void gcost(float w1,float w2,float b,float *dw1,float *dw2,float *db){
     *db/=lunghezza;
 }
 int main() {
-    srand(time(0));
+    srand(69);
     float w1 = numero(),w2 = numero(),b = numero();
     float dw1 = 0,dw2 = 0,db = 0;
 
