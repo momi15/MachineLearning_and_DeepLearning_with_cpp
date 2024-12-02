ef9e9ac6ec589d79fb12ec812800e6c8c849fe65
momi15 <hanitarek247@gmail.com>
2024-12-02 17:25:28 +0100

    Aggiornamenti automatici

--- ml/and or model/main.cpp
+++ ml/and or model/main.cpp
@@ -71,7 +71,7 @@ int main() {
     float dw1 = 0,dw2 = 0,db = 0;
 
     float eps = 1e-1;  // Small epsilon for numerical derivative
-    float rate = 1e-8;
+    float rate = 1e-5;
     cout<<"C="<<cost(w1, w2,b)<<endl;
     for(size_t i=0;i<10000000*1000;++i){
         gcost(w1,w2,b,&dw1,&dw2,&db);
